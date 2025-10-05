9df05438ddee4fe19d00e2ba7d7b316b9be44ef1
Mint <123512601+mint-ium@users.noreply.github.com>
2025-10-05 14:10:02 -0400

    FTC Dash
    192.168.43.1:8080/dash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTriggerVariable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTriggerVariable.java
@@ -19,14 +19,10 @@ public class ShooterTriggerVariable extends OpMode {
     @Override
     public void loop() {
 
-        if (gamepad1.left_trigger > 0.15) {
+        if (gamepad1.left_trigger > 0.05) {
             velocity = gamepad1.left_trigger * -1.0;
             shooter.setMotorSpeed(velocity);
-        } else {
-            shooter.setMotorSpeed(0);
-        }
-
-        if (gamepad1.right_trigger > 0.15) {
+        }else if(gamepad1.right_trigger > 0.05) {
             velocity = gamepad1.right_trigger;
             shooter.setMotorSpeed(velocity);
         } else {
